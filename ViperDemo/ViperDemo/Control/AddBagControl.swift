2e2ae8a5728c50c72c37be644f8f4157a05582de
Shreya <shreya@Shreyas-MacBook-Air-2.local>
2020-05-05 15:05:07 +0530

    MVVM control in Viper architecture

---
+++ ViperDemo/ViperDemo/Control/AddBagControl.swift
@@ -0,0 +1,21 @@
+//
+//  AddBagControl.swift
+//  ViperDemo
+//
+//  Created by Shreya on 30/04/20.
+//  Copyright © 2020 Shreya Pallan. All rights reserved.
+//
+
+import UIKit
+
+class AddBagControl: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
